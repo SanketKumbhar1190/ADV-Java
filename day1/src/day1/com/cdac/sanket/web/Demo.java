3bf83c1e90cec3c5463f9460a37069a087cc5f55
Sanket Kumbhar <sanketkumbhar1190@gmail.com>
2025-06-07 22:54:49 +0530

    refactor things to perfection

---
+++ day1/src/day1/com/cdac/sanket/web/Demo.java
@@ -0,0 +1,11 @@
+package day1.com.cdac.sanket.web;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello there ");
+System.out.println("hello again");
+	}
+
+}
